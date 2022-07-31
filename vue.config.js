be6107a18d736e571b648edf41f5dd919c8bdfe7
tangleiwork <tangleiwork@qq.com>
2022-07-31 14:58:02 +0800

    222

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath:process.env.NODE_ENV === "production" ? "/webpage" : "/",
+  publicPath:process.env.NODE_ENV === "production" ? "/webpage/" : "/",
   configureWebpack: {
     resolve: {
       alias: {},
